b363d1623e0d5acc8712a1de4fc4c24f181df3e5
Doheum Park <pirbak@gmail.com>
2021-06-28 11:44:10 +0900

    2021-06-28 3교시

--- src/main/resources/static/js/user/profile.js
+++ src/main/resources/static/js/user/profile.js
@@ -28,37 +28,42 @@ function changeMainProfile(iprofile) {
         .then(myJson => {
             switch(myJson.result) {
                 case 0:
-                    alert('메인 이미지 변경에 실패하였습니다.');
-                    break;
+                    alert('메인 이미지 변경에 실패하였습니다.'); break;
                 case 1:
                     setMainProfileIcon(iprofile);
+
+                    //section에 있는 프로필 이미지 변경
+                    const src = profileImgElem.src;
+                    const frontSrc = src.substring(0, src.lastIndexOf("/"));
+                    const resultSrc = `${frontSrc}/${myJson.img}`
+                    profileImgElem.src = resultSrc;
+
+                    //헤더에 있는 프로필 이미지 변경
+                    const headerProfileImgElem = document.querySelector('header .span__profile img');
+                    headerProfileImgElem.src = resultSrc;
                     break;
             }
         });
 }
-
+                           //현재 mainProfile로 바뀐 iprofile값 입니다.
 function setMainProfileIcon(iprofile) {
     profileImgParentList.forEach(item => {
         item.innerHTML = '';
-
         const itemIprofile = item.dataset.iprofile;
         if(iprofile !== itemIprofile) {
             const iElem = document.createElement('i');
             iElem.className = 'no-main-profile pointer fas fa-bell';
             item.append(iElem);
-
             addIElemEvent(iElem);
         }
     });
 }
-
-
 //모달창 띄우기
 profileImgElem.addEventListener('click', () => {
     modalElem.classList.remove('hide');
 });
-
 //모달창 닫기
 modalCloseElem.addEventListener('click', () => {
     modalElem.classList.add('hide');
-})
+    //location.reload();
+});
