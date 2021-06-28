622024371b60f51b97fab96f58479d9a37c5ccd6
Doheum Park <pirbak@gmail.com>
2021-06-28 16:50:42 +0900

    2021-06-28 7교시

--- src/main/resources/static/js/feed/reg.js
+++ src/main/resources/static/js/feed/reg.js
@@ -1,20 +1,41 @@
-const selectedImgListElem = document.querySelector('#selectedImgList');
-const imgArrElem = document.querySelector('#imgArr');
-imgArrElem.addEventListener('change', () => {
-    selectedImgListElem.innerHTML = '';
+const fileList = [];
+const ctntElem = document.querySelector('#ctnt');
+const selectImgArrElem = document.querySelector('#selectImgArr');
+const btnUploadElem = document.querySelector('#btnUpload');
+const displayImgListElem = document.querySelector('#displayImgList');
 
-    const fileList = imgArrElem.files;
+//이미지들이 선택되면 fileList에 추가하기
+selectImgArrElem.addEventListener('change', () => {
+    const files = selectImgArrElem.files;
+    for(let i=0; i<files.length; i++) {
+        fileList.push(files[i]);
+    }
+    displaySelectedImgArr();
+});
+
+//fileList에 추가 된 이미지들 디스플레이하기
+function displaySelectedImgArr() {
+    displayImgListElem.innerHTML = '';
+
     for(let i=0; i<fileList.length; i++) {
         const item = fileList[i];
-
         const reader = new FileReader();
         reader.readAsDataURL(item);
-
         //로드 한 후
         reader.onload = () => {
             const img = document.createElement('img');
+            img.addEventListener('click', () => {
+                fileList.splice(i, 1);
+                displaySelectedImgArr();
+            });
             img.src = reader.result;
-            selectedImgListElem.append(img);
+            displayImgListElem.append(img);
         };
     }
-});
+}
+
+//등록버튼 클릭시 (Ajax로 파일 업로드)
+btnUploadElem.addEventListener('click', () => {
+
+
+})
