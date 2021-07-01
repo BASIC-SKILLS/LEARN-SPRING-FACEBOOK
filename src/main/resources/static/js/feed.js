1896e373fb2a0e57b980aeb5f17cb6e76ddff3f8
Doheum Park <pirbak@gmail.com>
2021-07-01 12:42:20 +0900

    2021-07-01 4교시

--- src/main/resources/static/js/feed.js
+++ src/main/resources/static/js/feed.js
@@ -1,3 +1,23 @@
+function getDateTimeInfo(dt) {
+    const nowDt = new Date();
+    const targetDt = new Date(dt);
+
+    const nowDtSec = parseInt(nowDt.getTime() / 1000);
+    const targetDtSec = parseInt(targetDt.getTime() / 1000);
+
+    const diffSec = nowDtSec - targetDtSec;
+    if(diffSec < 120) {
+        return '1분 전';
+    } else if(diffSec < 3600) { //분 단위
+        return `${parseInt(diffSec / 60)}분 전`;
+    } else if(diffSec < 86400) { //시간 단위
+        return `${parseInt(diffSec / 3600)}시간 전`;
+    } else if(diffSec < 604800) { //일 단위
+        return `${parseInt(diffSec / 86400)}일 전`;
+    }
+    return targetDt.toLocaleString();
+}
+
 const feedObj = {
     limit: 5,
     itemLength: 0,
@@ -16,6 +36,7 @@ const feedObj = {
             itemContainer.classList.add('item');
 
             // 글쓴이 정보 영역
+            const regDtInfo = getDateTimeInfo(item.regdt);
             const topDiv = document.createElement('div');
             topDiv.classList.add('top')
             topDiv.innerHTML = `
@@ -23,7 +44,7 @@ const feedObj = {
                 <img src="/pic/profile/${item.iuser}/${item.mainProfile}">
             </div>
             <div>
-                <div>${item.writer}</div>
+                <div>${item.writer} - ${regDtInfo}</div>
                 <div>${item.location == null ? '' : item.location}</div>
             </div>
         `;
